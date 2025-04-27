4b168c9771bf760fedea96535d03bb1f976bd6ee
Yerko Opazo <yerk.opazo.h@gmail.com>
2025-04-27 11:35:47 +0200

    Delete Pokedex directory

--- Pokedex/script.js
+++
@@ -1,6 +0,0 @@
-var testButton = document.getElementById("myButton");
-
-testButton.addEventListener("mouseover, function (){
-    var list = testButton.classList; 
-    list.add(mouseOverClass)")
-    
